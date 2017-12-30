d180d4de42e0deb1490817d2a3ce49048928269e
Cherry Luo <csu20140909@gmail.com>
2017-12-30 16:07:29 +0800

    Added arguments parser using getopt

--- 1st/ch3/dateOperation.c
+++ 1st/ch3/dateOperation.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct DateInfo {
 	int tm_year;	// years since 1900
@@ -8,7 +9,7 @@ typedef struct DateInfo {
 	int tm_mday;	// day of month - [1, 31]
 	int tm_wday;	// days since Sunday - [0, 6]
 	int tm_yday;	// days since January 1 - [0, 365]
-};
+} DateInfo;
 
 int DateDifference(DateInfo from, DateInfo to);
 int weekdayNumofDate(const DateInfo date);
@@ -18,15 +19,34 @@ void usage(const char* prog)
 {
 	printf("Usage: %s [options] date1 [date2]\n"
 		"Option:\n"
+		"-h Show help\n"
 		"-d date1 date2 Compute the number of days from date1 to date2\n"
 		"-c date Print the calendar of date\n"
-		"-w date Print the weekday number of date\n");
+		"-w date Print the weekday number of date\n", prog);
 }
 
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
-	
-
+	int ch;
+	while((ch = getopt(argc, argv, "d:c:w:h")) != -1) {
+		switch(ch) {
+			case 'h' :
+			case ':' :
+			case '?' :
+			default :
+				usage(argv[0]);
+				break;
+			case 'd' :
+				printf("%s -d %s %s\n", argv[0], argv[optind-1], argv[optind]);
+				break;
+			case 'c':
+				printf("%s -c %s\n", argv[0], argv[optind-1]);
+				break;
+			case 'w':
+				printf("%s -w %s\n", argv[0], argv[optind-1]);
+				break;
+		}
+	}
 
 	return 0;
 }
