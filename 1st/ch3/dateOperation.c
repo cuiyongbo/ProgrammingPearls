1dc4cdab8fc19e34107987f7c385c5ec5f3dfa3a
Cherry Luo <csu20140909@gmail.com>
2017-12-30 18:09:27 +0800

    Fixed dateOperation.c - isInvalidInput

--- 1st/ch3/dateOperation.c
+++ 1st/ch3/dateOperation.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
 typedef struct DateInfo {
 	int tm_year;	// years since 1900
@@ -15,6 +16,8 @@ int DateDifference(DateInfo from, DateInfo to);
 int weekdayNumofDate(const DateInfo date);
 void printCalendarOfDate(const DateInfo date);
 
+void DateDifferenceParser(const char* from, const char* to);
+
 void usage(const char* prog)
 {
 	printf("Usage: %s [options] date1 [date2]\n"
@@ -22,7 +25,31 @@ void usage(const char* prog)
 		"-h Show help\n"
 		"-d date1 date2 Compute the number of days from date1 to date2\n"
 		"-c date Print the calendar of date\n"
-		"-w date Print the weekday number of date\n", prog);
+		"-w date Print the weekday number of date\n"
+		"Date format: yyyymmdd\n", prog);
+}
+
+
+// Date Format: YYYYmmdd, like 20171103, 20120504
+DateInfo parseDateFromString(const char* dateStr)
+{
+	DateInfo input;
+	int dateNum = atoi(dateStr);
+	input.tm_mday = dateNum % 100;
+	dateNum /= 100;
+	input.tm_mon = dateNum % 100;
+	dateNum /= 100;
+	input.tm_year = dateNum;
+	return input;
+}
+
+int isInvalidInput(const char* input)
+{
+	const char* p = input;
+	while(isdigit(*p) != 0)
+		++p;
+
+	return (p-input)==8 && *p==0;
 }
 
 int main(int argc, char* argv[])
@@ -37,7 +64,10 @@ int main(int argc, char* argv[])
 				usage(argv[0]);
 				break;
 			case 'd' :
-				printf("%s -d %s %s\n", argv[0], argv[optind-1], argv[optind]);
+				if(argv[optind]==NULL || argv[optind-1]==NULL)
+					usage(argv[0]);
+				else
+					DateDifferenceParser(argv[optind-1], argv[optind]);				
 				break;
 			case 'c':
 				printf("%s -c %s\n", argv[0], argv[optind-1]);
@@ -51,7 +81,24 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-int DateDiffenence(DateInfo from, DateInfo to)
+void DateDifferenceParser(const char* from, const char* to)
+{
+	// sanity check
+	if(isInvalidInput(from)==0 || isInvalidInput(to)==0) {
+		fprintf(stderr, "Invalid input(s)! DateFormat: yyyymmdd\n");
+		return;
+	}
+
+	DateInfo fromDate = parseDateFromString(from);
+	DateInfo toDate = parseDateFromString(to);
+	
+	int difference = DateDifference(fromDate, toDate);
+
+	printf("%d\n", difference);
+}
+
+
+int DateDifference(DateInfo from, DateInfo to)
 {
 
 	return 0;
