8b612b772e81934ae24c916ac28da83699a72dbf
Cherry Luo <csu20140909@gmail.com>
2018-01-13 16:52:45 +0800

    Add assert to decide if an array is sorted

--- 1st/ch4/bsearch.c
+++ 1st/ch4/bsearch.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define element_count(array) sizeof(array)/sizeof(array[0])
 
@@ -14,6 +15,9 @@ size_t bsearch_local(DataType key, DataType* arr, size_t count)
 
 	while(start <= end) {
 		size_t mid = start + (end-start)/2;
+		// make sure the array is sorted
+		assert(arr[start] <= arr[mid] && arr[mid] <= arr[end]);
+
 		if(arr[mid] < key) {
 			start = mid + 1;
 		} else if(arr[mid] == key) {
@@ -33,12 +37,12 @@ int main(int argc, char* argv[])
 	DataType key;
 	size_t count; 
 	size_t pos = -1;
-	while(scanf("%d %d", &count, &key) != EOF) {
+	while(scanf("%lu %d", &count, &key) != EOF) {
 		DataType* arr = (DataType*)malloc(count * sizeof(DataType));
 		for(int i = 0; i<count; ++i)
 			arr[i] = i*10;
 		pos = bsearch_local(key, arr, count);
-		printf("%d\n", pos);
+		printf("%lu\n", pos);
 		free(arr);
 	}
 
