3062367b691a8a2f032d3e4227be8650045d6a3b
Cherry Luo <csu20140909@gmail.com>
2018-01-21 19:15:35 +0800

    Added new testcases

--- 1st/appendix/timemod.c
+++ 1st/appendix/timemod.c
@@ -4,19 +4,19 @@
 #include <time.h>
 
 #define TRIALS	5
-#define LOOPCOUNT	5000
+#define LOOPCOUNT  5000
 
 #define T(s) printf("%s (trial = %d)\n", s, TRIALS)
 
 #define M(op)	{	\
-	printf("%-22s", #op);	\
+	printf("%-30s", #op);	\
 	clock_t timeSum = 0;	\
 	for (int ex=0; ex<TRIALS; ++ex) {	\
 		k = 0;	\
 		clock_t start = clock();	\
-		for(i=1; i<=LOOPCOUNT; ++i) {	\
+		for(i=1; i<=loops; ++i) {	\
 			fi = (float)i;	\
-			for(j=1; j<=LOOPCOUNT; ++j) { 	\
+			for(j=1; j<=loops; ++j) { 	\
 				fj = (float)j;	\
 				op;	\
 			}	\
@@ -29,6 +29,21 @@
 	printf("%8.0f\n", avg);	\
 }
 
+#define MAXN	100000
+int x[MAXN];
+
+int intcmp(int *i, int *j) { return *i - *j;}
+
+#define swapmac(i, j) {int t=x[i]; x[i]=x[j]; x[j]=t;}
+void swapfunc(int i, int j)
+{
+	int t = x[i];
+	x[i] = x[j];
+	x[j] = t;
+}
+
+#define maxmac(a, b) ((a)>(b)?(a):(b))
+int maxfunc(int a, int b) { return a>b?a:b; }
 
 
 int main()
@@ -40,6 +55,9 @@ int main()
 	float fi = 0;
 	float fj = 0;
 	float fk = 0;
+
+	int loops;
+	loops = LOOPCOUNT;
 
 	T("Integer Algorithm");
 	M({});
@@ -58,6 +76,49 @@ int main()
 	M(fk = fi - fj);
 	M(fk = fi * fj);
 	M(fk = fi / fj);
+
+	T("Array Operations");
+	for(int i=0; i<LOOPCOUNT; ++i)
+		x[i] = rand();
+
+	M(k = i + j);
+	M(k = x[i] + j;);
+	M(k = i + x[j];);
+	M(k = x[i] + x[j];);
+	
+	T("Comparisons");
+	M(if(i < j) k++);
+	M(if(x[i] < x[j]) k++);
+
+	T("Array Comparisons and Swaps");	
+	M(k = (x[i]<x[j]) ? -1 : 1);
+	M(k = intcmp(x+i, x+j));
+	M(swapfunc(i, j));
+	M(swapmac(i, j));
+	
+	T("Max Function, Macro, and Inline");
+	M(k = (i>j)? i : j);
+	M(k = maxmac(i, j));
+	M(k = maxfunc(i, j));
+
+	loops = LOOPCOUNT/5;
+
+	T("Math Functions");
+	M(k = rand());
+	M(fk = j + fi);
+	M(fk = sqrt(j+fi));
+	M(fk = sin(j+fi));
+	M(fk = sinh(j+fi));
+	M(fk = asin(j+fi));
+	M(fk = cos(j+fi));
+	M(fk = tan(j+fi));
+
+	loops = LOOPCOUNT/100;
+
+	T("Memory Allocation");
+	M(free(malloc(16)));
+	M(free(malloc(100)));
+	M(free(malloc(2000)));
 
 	return 0;
 }
