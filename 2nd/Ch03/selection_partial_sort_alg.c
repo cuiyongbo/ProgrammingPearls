2f54d17b7e4b29de38be38f5ce20c8861b522e92
cuiyb <cuiyb@mapbar.com>
2018-11-18 19:45:39 +0800

    added randomized selection algorithm

--- 2nd/Ch03/selection_partial_sort_alg.c
+++ 2nd/Ch03/selection_partial_sort_alg.c
@@ -7,6 +7,7 @@ int randint(int l, int u)
 	return (l+u)/2;
 }
 
+// find the kth element, zero-based.
 void select_nthElement(int* a, int l, int u, int k)
 {
 	if(l >= u)
@@ -27,6 +28,33 @@ void select_nthElement(int* a, int l, int u, int k)
 		select_nthElement(a, l, m-1, k);
 	else if(m<k)
 		select_nthElement(a, m+1, u, k);
+}
+
+
+// find the kth element, one-based.
+void select_nthElement_2(int* a, int l, int u, int k)
+{
+	if(l >= u)
+		return;
+
+	swapWithType(a[l], a[randint(l, u)], int);
+	int m = l;
+	for(int i=l+1; i<=u; i++)
+	{
+		if(a[i] < a[l])
+		{
+			++m;
+			swapWithType(a[i], a[m], int);
+		}
+	}
+	swapWithType(a[m], a[l], int);
+	int q = m-l+1;
+	if(q == k)
+		return;
+	if(q > k)
+		select_nthElement_2(a, l, m-1, k);
+	else if(q < k)
+		select_nthElement_2(a, m+1, u, k-q);
 }
 
 void select_nthElement_loop(int* a, int l, int u, int k)
@@ -58,7 +86,8 @@ int main()
 	int a[] = {1, 2, 4, 1, 5, 7, 3, 9};
 	int length = sizeof(a)/sizeof(a[0]);
 	//select_nthElement(a, 0, length-1, length/2);
-	select_nthElement_loop(a, 0, length-1, length-1);
+	select_nthElement_2(a, 0, length-1, length/2);
+	//select_nthElement_loop(a, 0, length-1, length-1);
 	for(int i=0; i<length; i++)
 		printf("%d ", a[i]);
 	printf("\n");
