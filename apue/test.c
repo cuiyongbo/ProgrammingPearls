da6bce99ffb270ba2398b52a6097541cdaba24b3
natsume <csu20140909@gmail.com>
2019-03-20 19:03:27 +0800

    added binary search implementation

--- apue/test.c
+++ apue/test.c
@@ -3,11 +3,12 @@
 
 int main()
 {
-	FILE* fp = fopen("sample", "w");
+	FILE* fp = fopen("sample", "wb");
 	if(fp == NULL)
 		err_sys("fopen error");
 
-	fprintf(fp, "Hello world\n");
+	//fprintf(fp, "Hello world\n");
+	fwrite("Hello world", 1,  strlen("Hello world"), fp);
 	fclose(fp);	
 	return 0;
 }
