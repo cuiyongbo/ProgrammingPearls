a315eca69d7b245fac0a2f3caed60a4c2a4035c6
Cherry Luo <csu20140909@gmail.com>
2019-09-28 13:40:17 +0800

    added helper functions; reviewed pselect

--- unp/lib/unix_api_wrapper.c
+++ unp/lib/unix_api_wrapper.c
@@ -1,5 +1,13 @@
 #include "unp.h"
 
+void* Malloc(size_t size)
+{
+    void* ptr = = malloc(size);
+    if (ptr == NULL)
+        err_sys("malloc error");
+    return ptr;
+}
+
 ssize_t Read(int fd, void *ptr, size_t nbytes)
 {
     ssize_t n = read(fd, ptr, nbytes);
@@ -12,4 +20,48 @@ void Write(int fd, const void *ptr, size_t nbytes)
 {
     if (write(fd, ptr, nbytes) != nbytes)
         err_sys("write error");
+}
+
+void Sigaddset(sigset_t *set, int signo)
+{
+    if (sigaddset(set, signo) == -1)
+        err_sys("sigaddset error");
+}
+
+void Sigdelset(sigset_t *set, int signo)
+{
+    if (sigdelset(set, signo) == -1)
+        err_sys("sigdelset error");
+}
+
+void Sigemptyset(sigset_t *set)
+{
+    if (sigemptyset(set) == -1)
+        err_sys("sigemptyset error");
+}
+
+void Sigfillset(sigset_t *set)
+{
+    if (sigfillset(set) == -1)
+        err_sys("sigfillset error");
+}
+
+int Sigismember(const sigset_t *set, int signo)
+{
+    int n = sigismember(set, signo);
+    if (n == -1)
+        err_sys("sigismember error");
+    return n;
+}
+
+void Sigpending(sigset_t *set)
+{
+    if (sigpending(set) == -1)
+        err_sys("sigpending error");
+}
+
+void Sigprocmask(int how, const sigset_t *set, sigset_t *oset)
+{
+    if (sigprocmask(how, set, oset) == -1)
+        err_sys("sigprocmask error");
 }
--- unp/unp.h
+++ unp/unp.h
@@ -8,6 +8,14 @@
 
 typedef struct sockaddr SA;
 
+// stdio api wrappers in stdio_wrapper.c
+FILE* Fopen(const char *filename, const char *mode);
+void Fclose(FILE *fp);
+FILE* Fdopen(int fd, const char *type);
+char * Fgets(char *ptr, int n, FILE *stream);
+void Fputs(const char *ptr, FILE *stream);
+
+// signal in signal.c
 typedef void (*sig_func_t)(int);
 sig_func_t Signal(int signo, sig_func_t func);
 
@@ -16,8 +24,16 @@ void sig_child(int signo);
 void str_echo(int sockFd);
 void str_cli(FILE* fp, int sockFd);
 
-// socket api wrappers in sock_wrapper.c
+// signal mask related api wrappers in unix_api_wrapper.c
+void Sigaddset(sigset_t *set, int signo);
+void Sigdelset(sigset_t *set, int signo);
+void Sigemptyset(sigset_t *set);
+void Sigfillset(sigset_t *set);
+int Sigismember(const sigset_t *set, int signo);
+void Sigpending(sigset_t *set);
+void Sigprocmask(int how, const sigset_t *set, sigset_t *oset);
 
+// socket api wrappers in sock_wrapper.c
 int Socket(int family, int type, int protocol);
 void Bind(int fd, const struct sockaddr *sa, socklen_t salen);
 void Listen(int fd, int backlog);
@@ -26,13 +42,16 @@ void Connect(int fd, const struct sockaddr *sa, socklen_t salen);
 
 void Setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen);
 
-int Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
-       struct timeval *timeout);
+int Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
 
-void Write(int fd, const void* ptr, size_t nbytes);
+ssize_t Readline(int fd, void* ptr, size_t maxlen);
 void Writen(int fd, const void* ptr, size_t nbytes);
 
-ssize_t Read(int fd, void *ptr, size_t nbytes);
-ssize_t Readline(int fd, void* ptr, size_t maxlen);
-
 void Shutdown(int fd, int how);
+
+char* Sock_ntop_host(const struct sockaddr *sa, socklen_t salen);
+
+// in unix_api_wrapper.c
+void* Malloc(size_t size);
+ssize_t Read(int fd, void *ptr, size_t nbytes);
+void Write(int fd, const void* ptr, size_t nbytes);
