79fcd70efe87bef5743668c65c94f621c99507c6
Cherry Luo <csu20140909@gmail.com>
2019-10-10 08:24:07 +0800

    added an example of preforked server with no lock around accept

--- unp/server/web_child.c
+++ unp/server/web_child.c
@@ -4,17 +4,14 @@
 
 void web_child(int sockfd)
 {
-    int         ntowrite;
-    ssize_t     nread;
     char        line[MAXLINE], result[MAXN];
-
     for ( ; ; )
     {
-        if ((nread = Readline(sockfd, line, MAXLINE)) == 0)
+        if (Readline(sockfd, line, MAXLINE) == 0)
             return;     /* connection closed by other end */
 
             /* 4line from client specifies #bytes to write back */
-        ntowrite = atol(line);
+        int ntowrite = atol(line);
         if ((ntowrite <= 0) || (ntowrite > MAXN))
             err_quit("client request for %d bytes", ntowrite);
 
