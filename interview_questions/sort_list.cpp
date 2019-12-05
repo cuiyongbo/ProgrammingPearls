6d779b8e807a22ad33199ad4cf6a957a40e2ff5d
Cherry Luo <csu20140909@gmail.com>
2019-12-05 22:10:25 +0800

    added linked list insertionSort

--- interview_questions/sort_list.cpp
+++ interview_questions/sort_list.cpp
@@ -4,16 +4,9 @@ using namespace std;
 
 class Solution {
 public:
-    ListNode* sortList(ListNode* head)
-    {
-        if(head == NULL || head->next == NULL)
-            return head;
-
-        ListNode* mid = partition(head);
-        ListNode* left = sortList(head);
-        ListNode* right = sortList(mid);
-        return merge(left, right);
-    }
+    ListNode* mergeSort(ListNode* head);
+    ListNode* insertionSortList(ListNode* head);
+    void reorderList(ListNode* head);
 
 private:
     ListNode* partition(ListNode* head)
@@ -61,6 +54,77 @@ private:
     }
 };
 
+ListNode* Solution::mergeSort(ListNode* head)
+{
+    if(head == NULL || head->next == NULL)
+        return head;
+
+    ListNode* mid = partition(head);
+    ListNode* left = mergeSort(head);
+    ListNode* right = mergeSort(mid);
+    return merge(left, right);
+}
+
+ListNode* Solution::insertionSortList(ListNode* head)
+{
+    ListNode dummy(0);
+    while(head != NULL)
+    {
+        int val = head->val;
+        ListNode* p = &dummy;
+        while(p->next != NULL && p->next->val < val)
+        {
+            p = p->next;
+        }
+
+        ListNode* next1 = p->next;
+        ListNode* next2 = head->next;
+
+        p->next = head;
+        head->next = next1;
+
+        head = next2;
+    }
+    return dummy.next;
+}
+
+void Solution::reorderList(ListNode* head)
+{
+    if(head == NULL || head->next == NULL)
+        return;
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while(fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    // split list into left and right sublist
+    // leftNodeCount >= rightNodeCount
+    ListNode* right = slow->next;
+    slow->next = NULL;
+
+    stack<ListNode*> s;
+    while(right != NULL)
+    {
+        s.push(right);
+        right = right->next;
+    }
+
+    ListNode* left = head;
+    while(!s.empty())
+    {
+        auto node = s.top(); s.pop();
+
+        ListNode* next = left->next;
+        left->next = node;
+        node->next = next;
+        left = next;
+    }
+}
+
 int main()
 {
     // string input = "[]";
@@ -71,7 +135,7 @@ int main()
     printLinkedList(ll);
 
     Solution ss;
-    ListNode* newLL = ss.sortList(ll);
+    ListNode* newLL = ss.mergeSort(ll);
     printLinkedList(newLL);
 
     destroyLinkedList(newLL);
