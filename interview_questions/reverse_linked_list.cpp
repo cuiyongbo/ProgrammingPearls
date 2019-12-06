758da309b723b9692cf1f2cfbda76069eced4513
Cherry Luo <csu20140909@gmail.com>
2019-12-06 22:28:49 +0800

    reviewed rotate list nodes between m and n

--- interview_questions/reverse_linked_list.cpp
+++ interview_questions/reverse_linked_list.cpp
@@ -17,55 +17,32 @@ public:
         return dummy.next;
     }
 
-    ListNode* reverseBetween(ListNode* head, int m, int n)
-    {
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* p = &dummy;
-
-        // find the (m-1)th node
-        for(int i=0; i<m-1; ++i)
-            p = p->next;
-
-        ListNode* prev = p;
-        ListNode* cur = p->next;
-        ListNode* tail = cur;
-        for(int i=m; i<=n; ++i)
-        {
-            ListNode* next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
-        }
-        p->next = prev;
-        tail->next = cur;
-        return dummy.next;
-    }
+    ListNode* reverseBetween(ListNode* head, int m, int n);
 
     ListNode* rotateRight(ListNode* head, int k) {
 
         if(k==0 || head == NULL || head->next == NULL)
             return head;
 
-        int n = 0;
+        int nodeCount = 0;
         ListNode* p = head;
         ListNode* tail = NULL;
         while(p != NULL)
         {
-            ++n;
+            ++nodeCount;
             tail = p;
             p = p->next;
         }
 
-        k %= n;
+        k %= nodeCount;
         if(k == 0) return head;
 
         ListNode dummy(0);
         dummy.next = head;
         p = &dummy;
 
-        // find the (n-k)th node
-        for(int i=0; i<n-k; ++i)
+        // find the (nodeCount-k)th node
+        for(int i=0; i<nodeCount-k; ++i)
         {
             p = p->next;
         }
@@ -78,22 +55,51 @@ public:
     }
 };
 
+ListNode* Solution::reverseBetween(ListNode* head, int m, int n)
+{
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode* p = &dummy;
+    for(int i=0; i<m-1; i++)
+        p = p->next;
+
+    ListNode* prev = p;
+    ListNode* cur = p->next;
+    ListNode* tail = cur;
+
+    for(int i=m; i<=n; i++)
+    {
+        ListNode* next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+
+    p->next = prev;
+    tail->next = cur;
+    return dummy.next;
+}
+
 
 int main()
 {
     string input = "[1,2,3,4,5]";
     //string input = "[]";
     ListNode* ll = stringToListNode(input);
+    cout << "original list: ";
     printLinkedList(ll);
 
     Solution ss;
     ListNode* newLL = ss.reverseList(ll);
+    cout << "reverseList: ";
     printLinkedList(newLL);
 
     newLL = ss.reverseBetween(newLL, 2, 4);
+    cout << "reverseBetween(newLL, 2, 4): ";
     printLinkedList(newLL);
 
     newLL = ss.rotateRight(newLL, 2);
+    cout << "rotateRight(newLL, 2): ";
     printLinkedList(newLL);
 
     destroyLinkedList(newLL);
