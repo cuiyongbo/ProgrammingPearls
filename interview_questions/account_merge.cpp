be64bceb479e9519d8a38998f3e084e393d2c930
Cherry Luo <csu20140909@gmail.com>
2019-12-16 23:31:33 +0800

    fixed account merger solution

--- interview_questions/account_merge.cpp
+++ interview_questions/account_merge.cpp
@@ -5,15 +5,22 @@ using namespace std;
 class Solution
 {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts)
-    {
-        int len = accounts.size();
-        vector<int> p(1001);
-        for(int i=0; i<p.size(); ++i) p[i] = i;
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts);
+};
+
+vector<vector<string>> Solution::accountsMerge(vector<vector<string>>& accounts)
+{
+        const int MAX_EMAIL_COUNT = 1000*10+1;
+        vector<int> p(MAX_EMAIL_COUNT);
+        for(int i=0; i<MAX_EMAIL_COUNT; ++i) p[i] = i;
 
         function<int(int)> find = [&](int x)
         {
-            return p[x] == x ? x : find(p[x]);
+            if(p[x] != x)
+            {
+                p[x] = find(p[x]);
+            }
+            return p[x];
         };
 
         function<void(int,int)> union_func = [&](int x, int y)
@@ -21,46 +28,61 @@ public:
             p[find(x)] = find(y);
         };
 
-        int mailId = 0;
-        unordered_map<string, int> email2NameId;
+        int accountId = 0, mailId = 0;
         unordered_map<string, int> email2MailId;
-        for(int i=0; i<len; ++i)
+        unordered_map<string, int> email2AccountId;
+        for(auto& account: accounts)
         {
-            for(int k=1; k<accounts[i].size(); ++k)
+            for(int k=1; k<account.size(); ++k)
             {
-                email2NameId[accounts[i][k]] = i;
-
-                if(email2MailId.find(accounts[i][k]) == email2MailId.end())
+                email2AccountId[account[k]] = accountId;
+                if(email2MailId.find(account[k]) == email2MailId.end())
                 {
-                    email2MailId[accounts[i][k]] = mailId++;
+                    email2MailId[account[k]] = mailId++;
                 }
-
-                union_func(email2MailId[accounts[i][1]], email2MailId[accounts[i][k]]);
+                union_func(email2MailId[account[1]], email2MailId[account[k]]);
             }
+            accountId++;
         }
 
         unordered_map<int, vector<string>> tmp;
-        for(auto& it: email2NameId)
+        for(auto& it: email2AccountId)
         {
             int id = find(email2MailId[it.first]);
             tmp[id].push_back(it.first);
-        }
-
-        for(auto& it: tmp)
-        {
-            sort(it.second.begin(), it.second.end());
-            it.second.erase(unique(it.second.begin(), it.second.end()), it.second.end());
         }
 
         int group = 0;
         vector<vector<string>> result(tmp.size());
         for(auto& it: tmp)
         {
-            auto& name = accounts[email2NameId[it.second.front()]][0];
+            auto& name = accounts[email2AccountId[it.second[0]]][0];
             result[group].push_back(name);
+
+            sort(it.second.begin(), it.second.end());
             result[group].insert(result[group].end(), it.second.begin(), it.second.end());
+
             ++group;
         }
         return result;
+}
+
+int main()
+{
+    vector<vector<string>> accounts {
+        {"John","johnsmith@mail.com","john_newyork@mail.com"},
+        {"John","johnsmith@mail.com","john00@mail.com"},
+        {"Mary","mary@mail.com"},
+        {"John","johnnybravo@mail.com"}
+    };
+
+    Solution ss;
+    vector<vector<string>> groups = ss.accountsMerge(accounts);
+
+    for(auto& group: groups)
+    {
+        cout << group[0] << ":\n";
+        for(int k=1; k<group.size(); ++k)
+            cout << "\t" << group[k] << "\n";
     }
-};
+}
