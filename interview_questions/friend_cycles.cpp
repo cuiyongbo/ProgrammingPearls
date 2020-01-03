bcd8a42a4a9af055c50736cf3fd4e1462b2f7efd
luo mai <csu20140909@gmail.com>
2020-01-03 20:15:05 +0800

    reviewed DSU

--- interview_questions/friend_cycles.cpp
+++ interview_questions/friend_cycles.cpp
@@ -11,38 +11,16 @@ public:
 int Solution::findCircleNum(vector<vector<int>>& M)
 {
     int size = M.size();
-    vector<int> p(size);
-    for(int i=0; i<size; i++) p[i] = i;
-
-    function<int(int)> find = [&] (int x) {
-        if(p[x] != x)
-        {
-            p[x] = find(p[x]);
-        }
-        return p[x];
-    };
-
-    function<void(int, int)> union_func = [&] (int x, int y)
-    {
-        p[find(x)] = find(y);
-    };
-
+    DSU dsu(size);
     for(int row=0; row<size; ++row)
     {
         for(int col=0; col<size; ++col)
         {
             if(M[row][col])
-                union_func(col, row);
+                dsu.unionFunc(col, row);
         }
     }
-
-    unordered_set<int> groups;
-    for(int row=0; row<size; ++row)
-    {
-        groups.emplace(find(row));
-    }
-
-    return groups.size();
+    return dsu.groupCount();
 }
 
 int Solution::findCircleNum_DFS(vector<vector<int>>& M)
