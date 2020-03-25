21db08098f033a793f8c2c32d84d15e01bcfab44
Cherry Luo <csu20120504@126.com>
2020-03-25 23:30:33 +0800

    add leetcode exercises: 98, 530, 449

--- interview_questions/binary_tree_serialization_and_deserializaiton.cpp
+++ interview_questions/binary_tree_serialization_and_deserializaiton.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 using namespace osrm;
 
+/* leetcode: 297, 449 */
 class BinaryTreeTextCodec
 {
 public:
@@ -57,7 +58,7 @@ TreeNode* BinaryTreeTextCodec::deserialize_workhorse(istringstream& in)
     }
 }
 
-void textCodec_scaffold(string input, string expectedResult)
+void BinaryTreeTextCodec_scaffold(string input, string expectedResult)
 {
     BinaryTreeTextCodec codec;
     TreeNode* expected = stringToTreeNode(expectedResult);
@@ -70,7 +71,7 @@ void textCodec_scaffold(string input, string expectedResult)
     }
 
     string s = codec.serialize(actual);
-    util::Log(logESSENTIAL) << s;
+    // util::Log(logESSENTIAL) << s;
     if(s != input)
     {
         util::Log(logERROR) << "BinaryTreeTextCodec::serialize failed";
@@ -80,9 +81,75 @@ void textCodec_scaffold(string input, string expectedResult)
     util::Log(logESSENTIAL) << "BinaryTreeTextCodec test passed";
 }
 
+class BSTCodec
+{
+public:
+    string serialize(TreeNode* root);
+    TreeNode* deserialize(string data);
+private:
+    TreeNode* deserialize_workhorse(string& data, int& pos, int curMin, int curMax);
+};
+
+string BSTCodec::serialize(TreeNode* root)
+{
+    string ans;
+    stack<TreeNode*> s;
+    if(root != NULL) s.push(root);
+    while(!s.empty())
+    {
+        auto t = s.top(); s.pop();
+        if(t->right != NULL) s.push(t->right);
+        if(t->left != NULL) s.push(t->left);
+        ans.append(reinterpret_cast<const char*>(&t->val), sizeof(t->val));
+        // util::Log(logESSENTIAL) << t->val;
+    }
+    return ans;
+}
+
+TreeNode* BSTCodec::deserialize(string data)
+{
+    int pos = 0;
+    return deserialize_workhorse(data, pos, INT_MIN, INT_MAX);
+}
+
+TreeNode* BSTCodec::deserialize_workhorse(string& str, int& pos, int curMin, int curMax)
+{
+    if(pos >= str.size()) return NULL;
+    int val = *reinterpret_cast<const int*>(str.data() + pos);
+    if(val < curMin || val > curMax) 
+        return NULL;
+
+    pos += sizeof(val);
+    TreeNode* root = new TreeNode(val);
+    root->left = deserialize_workhorse(str, pos, curMin, val);
+    root->right = deserialize_workhorse(str, pos, val, curMax);
+    return root;
+}
+
+void BSTCodec_scaffold(string input)
+{
+    TreeNode* root = stringToTreeNode(input);
+    
+    BSTCodec codec;
+    string encoded_str = codec.serialize(root);
+    TreeNode* decoded_tree = codec.deserialize(encoded_str);
+    if(binaryTree_equal(root, decoded_tree))
+    {
+        util::Log(logESSENTIAL) << "BSTCodec test passed";
+    }
+    else
+    {
+        util::Log(logERROR) << "BSTCodec test failed";
+    }
+    
+    destroyBinaryTree(root);
+    destroyBinaryTree(decoded_tree);
+}
+
 int main()
 {
     util::LogPolicy::GetInstance().Unmute();
 
-    textCodec_scaffold("1 2 4 # # # 3 # # ", "[1,2,3,4]");
+    BinaryTreeTextCodec_scaffold("1 2 4 # # # 3 # # ", "[1,2,3,4]");
+    BSTCodec_scaffold("[5,2,6,1,4,null,8]");
 }
---
+++ interview_questions/bst_traversal.cpp
@@ -0,0 +1,119 @@
+#include "leetcode.h"
+
+using namespace std;
+using namespace osrm;
+
+/* leetcode: 98, 530*/
+
+class Solution 
+{
+public:
+    bool isValidBST(TreeNode* root);
+    int getMinimumDifference(TreeNode* root);
+};
+
+bool Solution::isValidBST(TreeNode* root)
+{
+    function<bool(TreeNode*, TreeNode*, TreeNode*)>dfs = [&](TreeNode* node, TreeNode* maxNode, TreeNode* minNode)
+    {
+        if(node == NULL)
+        {
+            return true;
+        }
+        else if(maxNode != NULL && maxNode->val <= node->val)
+        {
+            return false;
+        }
+        else if(minNode != NULL && minNode->val >= node->val)
+        {
+            return false;
+        }
+        else
+        {
+            return dfs(node->left, node, minNode) && dfs(node->right, maxNode, node);
+        }
+    };
+
+    return dfs(root, NULL, NULL);
+}
+
+int Solution::getMinimumDifference(TreeNode* root)
+{
+    /*
+        Given a binary search tree with non-negative values, 
+        find the minimum absolute difference between values of any two nodes.
+    */
+
+    vector<int> inorderSeq;
+    function<void(TreeNode*)> dfs = [&](TreeNode* node)
+    {
+        if(node == NULL) return;
+        dfs(node->left);
+        inorderSeq.push_back(node->val);
+        dfs(node->right);
+    };
+
+    dfs(root);
+
+    int minDiff = INT_MAX;
+    for(int i=1; i<inorderSeq.size(); ++i)
+    {
+        minDiff = min(minDiff, abs(inorderSeq[i] - inorderSeq[i-1]));
+    }
+    return minDiff;
+}
+
+void isValidBST_scaffold(string input, bool expectedResult)
+{
+    TreeNode* root = stringToTreeNode(input);
+
+    Solution ss;
+    bool actual = ss.isValidBST(root);
+    if(actual == expectedResult)
+    {
+        util::Log(logESSENTIAL) << "Case (" << input << ", expected <" << expectedResult << ">) passed";
+    }
+    else
+    {
+        util::Log(logERROR) << "Case (" << input << ", expected<" << expectedResult << ">) failed";
+    }
+
+    destroyBinaryTree(root);
+}
+
+void getMinimumDifference_scaffold(string input, int expectedResult)
+{
+    TreeNode* root = stringToTreeNode(input);
+
+    Solution ss;
+    int actual = ss.getMinimumDifference(root);
+    if(actual == expectedResult)
+    {
+        util::Log(logESSENTIAL) << "Case (" << input << ", expected: " << expectedResult << ") passed";
+    }
+    else
+    {
+        util::Log(logERROR) << "Case (" << input << ", expected: " << expectedResult << ") failed";
+    }
+    destroyBinaryTree(root);
+}
+
+int main()
+{
+    util::LogPolicy::GetInstance().Unmute();
+
+    util::Log(logESSENTIAL) << "Running isValidBST tests:";
+    TIMER_START(isValidBST);
+    isValidBST_scaffold("[1,2,3]", false);
+    isValidBST_scaffold("[2,1,3]", true);
+    isValidBST_scaffold("[4,3,5,null,2]", false);
+    TIMER_STOP(isValidBST);
+    util::Log(logESSENTIAL) << "isValidBST using " << TIMER_MSEC(isValidBST) << " milliseconds.";
+
+    util::Log(logESSENTIAL) << "Running getMinimumDifference tests:";
+    TIMER_START(getMinimumDifference);
+    getMinimumDifference_scaffold("[1,2,3]", 1);
+    getMinimumDifference_scaffold("[5,2,6,0,4,null,8]", 1);
+    TIMER_STOP(getMinimumDifference);
+    util::Log(logESSENTIAL) << "getMinimumDifference using " << TIMER_MSEC(getMinimumDifference) << " milliseconds.";
+}
