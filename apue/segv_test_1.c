b75f90853137e484c9cdc5859fa9cf1c572abc06
natsume <csu20120504@126.com>
2020-04-24 20:22:52 +0800

    reviewed signal

--- apue/segv_test_1.c
+++ apue/segv_test_1.c
@@ -2,12 +2,13 @@
 
 void sigHandler(int signo)
 {
-    printf("%s received\n", strsignal(signo));
+    printf("PID<%d>: %s received\n", (int)getpid(), strsignal(signo));
     sleep(1);
 }
 
 int main()
 {
+    printf("main pid: %d\n", (int)getpid());
     if(signal(SIGSEGV, sigHandler) == SIG_ERR)
     {
         err_sys("signal error");
