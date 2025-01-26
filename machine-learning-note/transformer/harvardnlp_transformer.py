6b1be8725d2998f4f46601b29843a2c625c32dab
Cherry Luo <csu20120504@126.com>
2025-01-26 23:18:40 +0800

    add MultiHeadedAttention

--- machine-learning-note/transformer/harvardnlp_transformer.py
+++ machine-learning-note/transformer/harvardnlp_transformer.py
@@ -46,6 +46,69 @@ class EncoderDecoder(nn.Module):
         return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
 
 
+def clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+def attention(query, key, value, mask=None, dropout=None):
+    """
+    Compute Scaled Dot Product Attention
+    """
+    d_k = query.size(-1)
+    # query: nbatches, h, -1, d_k
+    # key: nbatches, h, -1, d_k
+    # key.transpose(-2, -1): nbatches, h, d_k, -1
+    # value: nbatches, h, -1, d_k
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask==0, -1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % h == 0
+        # assume d_v == d_k
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+        # do all the linear projections in batch from d_model ==> h x d_k
+        query, key, value = [
+            lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1,2)
+            for lin, x in zip(self.linears, (query, key, value))
+        ]
+        # apply attentionon all the projected vectors in batch
+        x, self.attn = attention(
+            query, key, value, mask=mask, dropout=self.dropout
+        )
+        # concatenate using a view and apply a final linear
+        x = x.transpose(1,2).contiguous().view(nbatches, -1, self.h*self.d_k)
+        del query
+        del key
+        del value
+        return self.linears[-1](x)
+
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
 
 def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
     c = copy.deepcopy
